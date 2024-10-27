140669a0c352b0371a8433d33ae2f3befa8ac65d
Jorge12 <0250990@up.edu.mx>
2024-10-27 16:15:05 -0600

    fibonacci done (1pt)

--- Portfolio/05-JS/js/fibonacci.js
+++ Portfolio/05-JS/js/fibonacci.js
@@ -1,27 +1,18 @@
-/*
-    Fibonacci Sequence - Enter a number and have the program
-    generate the Fibonacci sequence to that number or to the Nth number.
-*/
-// This array will keep memory of the previous fibonacci numbers
-var memo = {};
-function fibonacci() {
-  "use strict";
-  var n = document.getElementById("num").value;
-  var val = f(n);
-  return val;
-}
 
-function f(n) {
-  var value;
-  // Check if the memory array already contains the requested number
-  if (memo.hasOwnProperty(n)) {
-    value = memo[n];
+function fibonacci(n) {
+  if (n <= 1) {
+      return n;
   } else {
-    //TODO: Implement the fibonacci function here!
-
-    memo[n] = value;
+      return fibonacci(n - 1) + fibonacci(n - 2);
   }
-
-  return value;
 }
-console.log(fibonacci(15));
+
+function displayFibonacci() {
+  const num = document.getElementById("num").value;
+  const resultElement = document.getElementById("result");
+
+  const position = parseInt(num);
+  const fibonacciNumber = fibonacci(position);
+
+  resultElement.textContent = `Fibonacci number at position ${position} is: ${fibonacciNumber}`;
+}
